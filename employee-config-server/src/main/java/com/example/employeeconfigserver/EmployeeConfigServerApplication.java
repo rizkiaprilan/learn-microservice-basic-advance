c5ea26cb6475b1f5b22f6ccd2aab050feaf5620d
Rizki Aprilan <riskiazza@gmail.com>
2022-03-25 14:50:10 +0700

    Spring Cloud Tutorial - Publish Events Using Spring Cloud Bus
    Link: https://www.javainuse.com/spring/cloud-stream-bus

--- employee-config-server/src/main/java/com/example/employeeconfigserver/EmployeeConfigServerApplication.java
+++ employee-config-server/src/main/java/com/example/employeeconfigserver/EmployeeConfigServerApplication.java
@@ -1,10 +1,21 @@
 package com.example.employeeconfigserver;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.cloud.bus.event.RefreshRemoteApplicationEvent;
 import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.context.scope.refresh.RefreshScopeRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @EnableConfigServer
+@RefreshScope
 @SpringBootApplication
 public class EmployeeConfigServerApplication {
 
--- employee-producer/src/main/java/com/example/employeeproducer/EmployeeProducerApplication.java
+++ employee-producer/src/main/java/com/example/employeeproducer/EmployeeProducerApplication.java
@@ -2,16 +2,25 @@ package com.example.employeeproducer;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.cloud.bus.event.RefreshRemoteApplicationEvent;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.context.scope.refresh.RefreshScopeRefreshedEvent;
+import org.springframework.context.event.EventListener;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @EnableCircuitBreaker
 @EnableDiscoveryClient
+@RefreshScope
 @OpenAPIDefinition(info = @Info(title = "Employees API", version = "2.0", description = "Employees Information"))
-//@ComponentScan({"com.example.employeeproducer"})
 @SpringBootApplication
 public class EmployeeProducerApplication {
 
@@ -19,4 +28,28 @@ public class EmployeeProducerApplication {
         SpringApplication.run(EmployeeProducerApplication.class, args);
     }
 
+}
+@Controller
+@ResponseBody
+class PropertyHttpController {
+
+    @Autowired
+    Environment environment;
+
+    @GetMapping("/value")
+    String read() {
+        return this.readValue();
+    }
+
+    @EventListener({
+            RefreshRemoteApplicationEvent.class,
+            ApplicationReadyEvent.class,
+            RefreshScopeRefreshedEvent.class})
+    public void refresh() {
+        System.out.println("the new value is " + this.readValue());
+    }
+
+    private String readValue() {
+        return this.environment.getProperty("eureka.client.serviceUrl.defaultZone");
+    }
 }
