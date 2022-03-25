d2a0bc0098cb567f767c55be1a0076ee957dac14
Rizki Aprilan <riskiazza@gmail.com>
2022-03-25 21:23:11 +0700

    Spring Cloud Tutorial - Distributed Log Tracing using Sleuth and Zipkin Example
    Link: https://www.javainuse.com/spring/cloud-sleuth

---
+++ learn-sleuth-and-zipkin/microservice4/src/main/java/com/example/microservice4/Microservice4Application.java
@@ -0,0 +1,47 @@
+package com.example.microservice4;
+
+
+import brave.sampler.Sampler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class Microservice4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Microservice4Application.class, args);
+    }
+
+}
+@RestController
+class Microservice4Controller {
+
+    private final Logger LOG = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    RestTemplate restTemplate;
+
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
+
+    @Bean
+    public Sampler defaultSampler() {
+        return Sampler.ALWAYS_SAMPLE;
+    }
+
+    @GetMapping(value = "/microservice4")
+    public String method4() {
+        LOG.info("Inside method4");
+        return "Hello World JavaInUse";
+    }
+}
+
