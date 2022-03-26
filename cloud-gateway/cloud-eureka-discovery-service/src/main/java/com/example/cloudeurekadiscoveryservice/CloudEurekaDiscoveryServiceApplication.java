53fc2ade4bbc0e66b0a7bdff6cba8d5e5bfc8c39
Rizki Aprilan <riskiazza@gmail.com>
2022-03-26 19:03:08 +0700

    Spring Cloud Tutorial - Spring Cloud Gateway + Netflix Eureka Example
    Link: https://www.javainuse.com/spring/cloud-gateway-eureka

---
+++ cloud-gateway/cloud-eureka-discovery-service/src/main/java/com/example/cloudeurekadiscoveryservice/CloudEurekaDiscoveryServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.cloudeurekadiscoveryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class CloudEurekaDiscoveryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudEurekaDiscoveryServiceApplication.class, args);
+    }
+
+}
---
+++ cloud-gateway/cloud-eureka-discovery-service/src/test/java/com/example/cloudeurekadiscoveryservice/CloudEurekaDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudeurekadiscoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudEurekaDiscoveryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
